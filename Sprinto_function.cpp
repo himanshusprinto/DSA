530d190333849fece28178eff58ef9724195cde7
himanshusprinto <64261380+himanshusprinto@users.noreply.github.com>
2021-03-21 16:27:53 +0530

    cp file

---
+++ Sprinto_function.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+bool solve()
+{
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        cout << (solve() ? "YES\n" : "NO\n");
+    }
+}
